1c7db8789b06be0af78301af954fed191c6ab704
mainvoid00 <133748782+mainvoid00@users.noreply.github.com>
2023-08-23 17:02:01 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 0 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.cc
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+        for(int k=1;k<=i*2-1;k++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    for(int i=1;i<=n-1;i++){
+        for(int j=1;j<=i;j++){
+            cout<<" ";
+        }
+        for(int k=1;k<=2*n-2*i-1;k++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
