95b0f921c164f02914c21c0a6a301b587c36afd7
mainvoid00 <133748782+mainvoid00@users.noreply.github.com>
2024-01-17 05:41:13 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 4 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    for(int i=0;i<100;i++)
+    {
+        string a;
+        getline(cin,a);
+        cout<<a<<"\n";
+    }
+    return 0;
+}
