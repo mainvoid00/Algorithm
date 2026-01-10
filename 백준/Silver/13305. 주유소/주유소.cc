3bed124e2c1233783d622937cd156a94b09e0908
mainvoid00 <133748782+mainvoid00@users.noreply.github.com>
2026-01-10 09:23:39 +0900

    [Silver III] Title: 주유소, Time: 36 ms, Memory: 2672 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.cc
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -1,33 +1,30 @@
 #include <stdio.h>
 
-int distance[100000];
-int cost[100001];
+long long distance[100005];
+long long cost[100005];
 
 int main(void)
 {
     int n;
-    int min_cost=10001;
-    int total_cost=0;
+    long long min_cost;
+    long long total_cost=0;
     scanf("%d", &n);
     for(int i=0;i<n-1;i++){
-        int temp;
-        scanf("%d", &temp);
-        distance[i]=temp;
+        scanf("%lld", &distance[i]);
     }
     
     for(int i=0;i<n;i++){
-        int temp;
-        scanf("%d" ,&temp);
-        cost[i]=temp;
+        scanf("%lld" ,&cost[i]);
     }
     
+    min_cost=cost[0];
     for(int i=0;i<n-1;i++){
         if(cost[i]<min_cost)
             min_cost=cost[i];
         total_cost= total_cost + min_cost*distance[i];
     }
     
-    printf("%d", total_cost);
+    printf("%lld", total_cost);
     
     return 0;
 }
